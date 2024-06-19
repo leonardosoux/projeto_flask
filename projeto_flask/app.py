1c245ecba21bdf6b35fba3d4d08daecba1282aef
Leonardo Souza Gomes <leonardosouza.gomes123@gmail.com>
2024-06-19 17:52:36 -0300

    formulários-cadastro

--- projeto_flask/app.py
+++ projeto_flask/app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template #(importação da classe Flask)
+from flask import Flask, render_template, request, redirect, url_for 
 
 lista_produtos = [ 
-    {"nome": "Coca-cola", "descricao": "veneno"},
-    {"nome": "Doritos", "descricao": "suja mão"},
-    {"nome": "Água", "descricao": "mata sede"},
+    {"nome": "Coca-cola", "descricao": "veneno", "preco": "10,00", "imagem": "https://www.coca-cola.com.br/assets/images/home/no-sugar/mobile/414w/no-sugar-banner.png?v=7a2730c7709c993e22636bc201ef6118" },
+    {"nome": "Doritos", "descricao": "suja mão","preco": "7,00", "imagem": "https://http2.mlstatic.com/D_NQ_NP_843674-MLB75571159538_042024-O.webp" },
+    {"nome": "Água", "descricao": "mata sede", "preco": "2,50", "imagem": "https://fontagua.com.br/wp-content/uploads/2019/02/FONTAGUA_GARRAFA-350ML-GAS.jpg"},
 ]
 
 app = Flask("minha app")
@@ -27,4 +27,19 @@ def produto(nome):
             return render_template("produto.html", produto=produto) 
     return "produto não existe"
 
-app.run
+# GET
+@app.route("/produtos/cadastro")
+def cadastro_produto():
+    return render_template("cadastro_produto.html")
+
+# POST
+@app.route("/produtos", methods=["POST"])
+def salvar_produto():
+    nome = request.form['nome']
+    descricao = request.form['descricao']
+    preco = request.form['preco']
+    imagem = request.form['imagem']
+    produto = {"nome": nome, "descricao": descricao, "preco": preco, "imagem": imagem}
+    lista_produtos.append(produto)
+
+    return redirect(url_for("produtos"))
